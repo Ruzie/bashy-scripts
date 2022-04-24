f7bd04c2df0cc0af75d0190c57b1d4c9c5c9b28b
Dale <luhamailbox@gmail.com>
2022-04-25 00:59:39 +0530

    Added arch.sh

---
+++ arts/arch.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+init_colors() {
+  red="\033[91m"
+  green="\033[92m"
+  yellow="\033[93m"
+  blue="\033[94m"
+  violet="\033[95m"
+  auqa="\033[96m"
+  end="\033[0m"
+}
+init_colors
+echo "${red}${USER} ~> A GNU/Linux user!"
+echo "${red}          ■■■    ${end}"        "${blue}              ■■■    ${end}"        "${green}              ■■■    ${end}"    "${red}              ■■■  ${end}"
+echo "${green}        █     █ ${end}"       "${red}             █     █ ${end}"      "${red}             █     █ ${end}"         "${green}             █    █ ${end}"
+echo "${yellow}       █       █ ${end}"     "${yellow}           █       █ ${end}"     "${violet}           █       █ ${end}"    "${yellow}           █      █ ${end}"
+echo "${blue}      █         █ ${end}"      "${green}         █         █ ${end}"       "${auqa}         █         █ ${end}"     "${auqa}         █        █ ${end}"
+echo "${violet}     █           █ ${end}"   "${auqa}       █           █ ${end}"     "${yellow}       █           █ ${end}"      "${blue}       █          █ ${emd}"
+echo "${auqa}    █    ■  ■     █ ${end}"    "${green}     █    ■  ■     █ ${end}"       "${blue}     █    ■  ■     █ ${end}"     "${violet}     █    ■  ■    █ ${end}" 
+echo "${green}   █   ■       ■   █ ${end}"  " ${violet}  █   ■       ■   █ ${end}"      " ${red}  █   ■       ■   █ ${end}"      "${green}   █   ■      ■   █ ${end}"
+echo "${red}  █ ■             ■ █ ${end}"   "${green} █ ■             ■ █ ${end}"        "${green} █ ■             ■ █ ${end}"   "${red} █ ■            ■ █ ${end}"
